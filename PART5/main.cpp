5eb0a86fb98c41d56e6752552a96e6092e9b91aa
linchen2508 <59896826+linchen2508@users.noreply.github.com>
2021-02-20 13:04:09 -0500

    Update main.cpp

--- PART5/main.cpp
+++ PART5/main.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
 #include <string>
 #include "BiddingFacility.h"
+#include <vector>
 using namespace std;
 
 int main() 
 {
-	BiddingFacility one("Lin", 100, 10);
-	BiddingFacility two("Sin", 100, 10);
-	BiddingFacility three("Seg", 100, 30);
+	BiddingFacility a("Alex", 100, 20);
+	BiddingFacility b("Bob", 120, 60);
+	BiddingFacility c("Chad", 140, 40);
+	BiddingFacility d("Dean", 160, 10);
 
-	BiddingFacility testArray[] = { one, two, three };
+	BiddingFacility* one = new BiddingFacility("Zin", 100, 50);
+	BiddingFacility* two = new BiddingFacility("Lone", 100, 30);
+	BiddingFacility* three = new BiddingFacility("Seg", 100, 0);
+	BiddingFacility* four = new BiddingFacility("Ore", 100, 50);
 
-	BiddingFacility::compareBid(testArray);
+	//Test the assignment operator
+	BiddingFacility* AOTest;
+	AOTest = three;
+
+	cout << AOTest;
+
+	//Test the copy constructor
+	BiddingFacility* CCTest = four;
+
+	//Test the stream insertion operator
+	cout << four;
+	cout << a;
+	cout << b;
+	cout << c;
+	cout << d;
+
+	vector<BiddingFacility*> testVector;
+
+	testVector.push_back(one);
+	testVector.push_back(two);
+	testVector.push_back(three);
+	testVector.push_back(four);
+
+	//Test the function to see who goes first
+	BiddingFacility::compareBid(testVector);
+
+	//Destructor will automatically be called after execution finish
+
+
+
+
 }
