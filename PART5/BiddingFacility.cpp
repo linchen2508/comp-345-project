92954241fcdf1d7e057be3fc6b7fc1557f15695b
linchen2508 <59896826+linchen2508@users.noreply.github.com>
2021-02-20 13:03:50 -0500

    Update BiddingFacility.cpp

--- PART5/BiddingFacility.cpp
+++ PART5/BiddingFacility.cpp
@@ -26,13 +26,13 @@ BiddingFacility& BiddingFacility::operator=(const BiddingFacility& object) {
 	this->bid = object.bid;
 	return *this;
 }
-
+//reveal the bid of each player
 void BiddingFacility::revealBid()
 {
 	cout << name << " bid " << bid;
 }
-
-void BiddingFacility::setBid() 
+//set the bid for each player
+void BiddingFacility::setBid()
 {
 	cout << "Enter the amount you wanna bid: ";
 	cin >> bid;
@@ -42,34 +42,64 @@ void BiddingFacility::setBid()
 		cin >> bid;
 	}
 }
-
+//get the bid of each player
 int BiddingFacility::getBid()
 {
 	return bid;
 }
-
-void BiddingFacility::compareBid(BiddingFacility a[])
+//Method to find out who goes first
+void BiddingFacility::compareBid(vector<BiddingFacility*> a)
 {
 	int max = 0;
-	string currentmaxname = "";
-	for (int i = 0; i < sizeof(a); i++)
-	{
-		if (a[i].bid > max)
-			max = a[i].bid;
-	}
-
-	for (int i = 0; i < sizeof(a); i++)
-	{
-		if (a[i].bid == max && a[i].name.compare(currentmaxname) > 0)
-			currentmaxname = a[i].name;
-	}
-	cout << currentmaxname << " bids the most coins and will go first";
-}
-	//Some shitty ass functions for the winner to pay coin to the coin supply below
-	//
-	//........
+	string currentMaxName=a[0]->name;
+	size_t arraylength = sizeof(a) / sizeof(a[0]);
+	bool sameBid = false;
 	
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		
+		cout << a[i]->name << " has bid " << a[i]->bid << endl;
+		
+		if (a[i]->bid > max)
+		{
+			max = a[i]->bid;
+			currentMaxName = a[i]->name;
+		}
+		
+	}
+	
+	for (int i = 0; i < a.size(); i++)
+	{
+		
+		if (a[i]->bid == max && a[i]->name.compare(currentMaxName) < 0)
+		{
+			currentMaxName = a[i]->name;
+			sameBid = true;
+		}
 
+	}
+	if (max == 0)
+		cout << "\nAll player bids 0 so " << currentMaxName << " will go first because his name is first in alphabetical order " << endl;
+	else
+		if (sameBid)
+			cout << "\nTwo or more players have the same bid but " << currentMaxName << " will go first becasue his name is first in alphabetical order " << endl;
+		else
+		cout << "\n" << currentMaxName << " will go first due to highest bid: " << max;
+}
 
+//stream insertion operator
+ostream& operator<<(ostream& output, const BiddingFacility& BF)
+{
+	output << "Name: " << BF.name << ", Money: " << BF.money << ", Bid: " << BF.money << endl;
+	return output;
+}
+
+//Destructor
+BiddingFacility::~BiddingFacility() {
+	cout << "Destructor is called" << endl;
+	name = "";
+	money = 0;
+	bid = 0;
+}
 
 
